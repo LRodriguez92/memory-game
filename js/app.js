11a354426aae4ba0a9216164c7e6221018bded28
student_123 <student_123@ool-ad020780.dyn.optonline.net>
2018-10-23 20:31:00 -0400

    added function to randomize images

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const board = document.querySelector('#board');
 const images = ['images/ifrit.png', 'images/cloud.png', 'images/sharingan.png', 'images/rinnegan.png', 'images/sephiroth.png',
 ];
 let levelImages = [];
+let randomImages = [];
+
 
 
 const level1 = {
@@ -25,9 +27,41 @@ const level2 = {
 
  };
 
-function renderBoard(level) {
-  let idNum = 0; // used to add number in id names
+function renderImages() {
+  for (let i = 0; i < randomImages.length; i++) {
+    let cell = document.querySelector(`#cell${i}`);
+    let img = document.createElement('img');
+    img.setAttribute('src', randomImages[i]);
+    img.style.width = '100%';
+    img.style.height = '100%';
+    cell.appendChild(img);
+  }
+}
 
+// Doubles the images in the array
+function doubleImageArr(level) {
+  for (let i = 0; i < level.images; i++) {
+    levelImages.push(images[i]);
+  }
+  for (let i = 0; i < level.images * 2; i++) {
+    if (i < level.images) {
+      levelImages.push(images[i]);
+    }
+  }
+}
+
+// Randomizes images from levelImages into new array
+function randomizeImages(level) {
+  while (levelImages.length > 0) {
+    let randNum = Math.floor(Math.random() * levelImages.length);
+    let removed = levelImages.splice(randNum, 1);
+    randomImages.push(removed);
+    console.log(`${removed} was removed`);
+  }
+}
+
+function renderBoard(level) {
+  let idNum = 0; // used to add number to id names
   for (let i = 0; i < level.board.length; i++) {
     for (let j = 0; j < level.board[i].length; j++) {
       if (level.board[i][j] === 1) {
@@ -52,32 +86,8 @@ function renderBoard(level) {
     }
   }
   doubleImageArr(level);
-  console.log(levelImages);
-  // pairImages = images;
-  // for (let i = 0; i < level.images * 2; i++) {
-  //   if (i < level.images) {
-  //     pairImages.push(images[i]);
-  //   }
-
-    // let cell = document.querySelector(`#cell${i}`);
-    // let img = document.createElement('img');
-    // img.setAttribute('src', );
-    // img.style.width = '100%';
-    // img.style.height = '100%';
-    // cell.appendChild(img);
-  // }
-}
-
-// Doubles the images in the array
-function doubleImageArr(level) {
-  for (let i = 0; i < level.images; i++) {
-    levelImages.push(images[i]);
-  }
-  for (let i = 0; i < level.images * 2; i++) {
-    if (i < level.images) {
-      levelImages.push(images[i]);
-    }
-  }
+  randomizeImages(level);
+  renderImages();
 }
 
 renderBoard(level1);
